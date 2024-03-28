dac3f2487ff3d9c1661abd7c779325b04dd64daf
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2024-03-28 14:33:46 +0300

    added postgresql settings

--- trucktrekker/settings.py
+++ trucktrekker/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = "trucktrekker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'USER': os.getenv('DB_USER'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+
+    }
+}
 
 
 # Password validation
